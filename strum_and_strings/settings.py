8b2b293169d570a26b6f4eb1ae114282f4595577
seanpatton1 <seanpatton1@outlook.com>
2025-06-08 11:32:46 +0100

    amended email code in settings due to errors further testing

--- strum_and_strings/settings.py
+++ strum_and_strings/settings.py
@@ -124,7 +124,7 @@ if 'DATABASE_URL' in os.environ:
     EMAIL_USE_TLS = True
     EMAIL_HOST_USER = 'apikey'
     EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_API_KEY')
-    DEFAULT_FROM_EMAIL = os.environ.get('strumandstrings@outlook.com')
+    DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
 else:
     # Development settings
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
