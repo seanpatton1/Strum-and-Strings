0e3c0135a373b3c754bdace54dfc4d47c987338f
seanpatton1 <seanpatton1@outlook.com>
2025-06-12 19:12:50 +0100

    Force Cloudinary to use HTTPS

--- strum_and_strings/settings.py
+++ strum_and_strings/settings.py
@@ -205,6 +205,7 @@ CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+    'SECURE': True,
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
