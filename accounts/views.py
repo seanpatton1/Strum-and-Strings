6295d09729c5b858db93a81485a9b9986f744b37
seanpatton1 <seanpatton1@outlook.com>
2025-06-14 18:25:14 +0100

    completed almost all styling for admin pagesand edited html for each page

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.contrib import messages
 from django.http import JsonResponse
+from django.core.paginator import Paginator
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
@@ -58,11 +59,23 @@ def order_list(request):
 
 @staff_member_required
 def product_catalog(request):
-    products = Product.objects.all()
     categories = ['Acoustic', 'Electric', 'Accessories']
+    paginated_products = {}
+
+    for category in categories:
+
+        products = Product.objects.filter(category__name=category)
+
+        paginator = Paginator(products, 5)
+
+        page_number = request.GET.get(f'page_{category}', 1)
+        page_obj = paginator.get_page(page_number)
+
+        paginated_products[category] = page_obj
+
     return render(request, 'accounts/product_catalog.html', {
-        'products': products,
         'categories': categories,
+        'paginated_products': paginated_products,
     })
 
 
